d0ccc5f30da3639c377153bcb4579543659cfd9d
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-04-23 12:24:52 +0400

    declare string paranteces

--- src/ParenthesesValidator.java
+++ src/ParenthesesValidator.java
@@ -1,3 +1,15 @@
 import java.util.Stack;
+
 public class ParenthesesValidator {
+    public static void main(String[] args) {
+        String test1 = "{[]()}";
+        String test2 = "[]]";
+        String test3 = "{}()";
+        String test4 = "";
+        String test5 = "(()[]{{}}))";
+
+    }
+
+
+
 }
