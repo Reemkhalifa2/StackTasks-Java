f522ce56ede02d8ac112a053ce3dee6d979c8b51
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-04-23 10:54:14 +0400

    Display the current stack with their positions from top

--- src/EmployeeStackSearch.java
+++ src/EmployeeStackSearch.java
@@ -1,4 +1,19 @@
-import  java.util.Stack;
+import java.util.Stack;
 public class EmployeeStackSearch {
+    public static void main(String[] args) {
+        Stack<String> empActions = new Stack<>();
+        //Add at least 5 employee action records using the push() method
+        empActions.push("Start work");
+        empActions.push("Meating");
+        empActions.push("Lunch Break");
+        empActions.push("Writing report");
+        empActions.push("leave work");
+
+        //Display the current stack with their positions from top
+        for(String action : empActions){
+            Integer position =  empActions.search(action);
+            System.out.println(action+ " : "+ position);
+        }
+    }
 
 }
