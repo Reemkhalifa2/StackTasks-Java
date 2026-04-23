6efedd40b087dec46fc06188d204d96274a3cded
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-04-23 11:19:37 +0400

    Using Pop() to remove recorde

--- src/BrowserHistorySimulator.java
+++ src/BrowserHistorySimulator.java
@@ -1,6 +1,21 @@
 import java.util.Stack;
 public class BrowserHistorySimulator {
     public static void main(String[] args) {
+        Stack<String> urlsStack = new Stack<>();
+        //Simulate visiting at least 5 different web pages using push()
+        urlsStack.push("www.github.com");
+        urlsStack.push("www.google.com");
+        urlsStack.push("www.instagram.com");
+        urlsStack.push("www.facebook.com");
+        urlsStack.push("www.youtube.com");
+        //Display the current page and all pages in history after each vis
+        System.out.println(urlsStack);
+        //Simulate pressing the "Back" button 2 times using pop()
+        urlsStack.pop();
+
+
+        urlsStack.pop();
+
 
     }
 }
