e8a7d8fd5584f39f224ca7d3a100a8d02071983c
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-04-23 12:25:40 +0400

    Implement is balanced method

--- src/ParenthesesValidator.java
+++ src/ParenthesesValidator.java
@@ -10,6 +10,24 @@ public class ParenthesesValidator {
 
     }
 
-
-
+    public static String isBalanced(String str) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch == '(' || ch == '[' || ch == '{') {
+                stack.push(ch);
+            }
+            else {
+                if (stack.isEmpty()) return "Not Balanced";
+                if (ch == ')' && stack.peek() == '(' ||
+                        ch == ']' && stack.peek() == '[' ||
+                        ch == '}' && stack.peek() == '{') {
+                    stack.pop();
+                } else {
+                    return "Not Balanced";
+                }
+            }
+        }
+        return stack.isEmpty() ? "Balanced" : "Not Balanced";
+    }
 }
