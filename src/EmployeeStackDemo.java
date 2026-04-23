675b86ba619cdf6e80fdadd8a14960196835db92
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-04-23 10:43:16 +0400

    Add record to the stack

--- src/EmployeeStackDemo.java
+++ src/EmployeeStackDemo.java
@@ -2,6 +2,13 @@ import java.util.Stack;
 public class EmployeeStackDemo {
     public static void main(String[] args) {
         Stack<String> empActions = new Stack<>();
+        //Add at least 5 employee action records using the push() method
+        empActions.push("Start work");
+        empActions.push("Meating");
+        empActions.push("Lunch Break");
+        empActions.push("Writing report");
+        empActions.push("leave work");
+
 
     }
 
