8108692d3b0ceb975e49c54c48888467a4e6bd3f
ReemALadawi <aladawi12reem6@gmail.com>
2026-04-25 12:25:40 +0400

    Complete reverseString method with pop logic to build reversed string

--- src/StringReversalAndPalindrome.java
+++ src/StringReversalAndPalindrome.java
@@ -1,14 +1,21 @@
 import java.util.*;
 public class StringReversalAndPalindrome {
     public static void main(String[] args) {
-
+        System.out.println(reverseString("Hello"));
     }
     public static String reverseString(String str){
-        Stack<String> reverse = new Stack<>();
+        Stack<Character> reverse = new Stack<>();
         String result = "";
         for(int i=0 ; i<str.length(); i++){
-
+            reverse.push(str.charAt(i));
         }
+
+        while(!reverse.isEmpty()){
+                result += reverse.pop();
+            }
+
+        return result;
+
 
     }
 
