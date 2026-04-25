f2d4cbaea0979da94d83af7d6833c677226041b1
ReemALadawi <aladawi12reem6@gmail.com>
2026-04-25 12:40:07 +0400

    test case one

--- src/StringReversalAndPalindrome.java
+++ src/StringReversalAndPalindrome.java
@@ -1,7 +1,19 @@
 import java.util.*;
 public class StringReversalAndPalindrome {
     public static void main(String[] args) {
-        System.out.println(reverseString("Hello"));
+        String str1 = "Hello";
+        String str2= "Hi";
+        String str3 = "all";
+        String str4 = "Mom";
+        String str5 = "LOL";
+        System.out.println("---------- Test Case 1 ----------------");
+        System.out.println("String " + str1);
+        System.out.println("Reversed String " + reverseString(str1));
+        System.out.println("Palindrome Status  " + isPalindrome(str1));System.out.println("String " + str1);
+        System.out.println("Reversed String " + reverseString(str1));
+        System.out.println("Palindrome Status  " + isPalindrome(str1));
+
+
     }
     public static String reverseString(String str){
         Stack<Character> reverse = new Stack<>();
