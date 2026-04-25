8c0c01cd9b0d298926b7bee6bcf4503131edd322
ReemALadawi <aladawi12reem6@gmail.com>
2026-04-25 23:44:04 +0400

    type(String text) - Add new text

--- src/TextEditorWithUndoRedo.java
+++ src/TextEditorWithUndoRedo.java
@@ -1,3 +1,17 @@
 import java.util.Stack;
 public class TextEditorWithUndoRedo {
+    public static void main(String[] args) {
+           }
+    public static String type(String text){
+        Stack<String>  stack = new Stack<>();
+        stack.push(text);
+    }
+    public static String type(String tesxt){
+        Stack<String> undoStack = new Stack<>();
+        while()
+    }
+    public static String type(String tesxt){
+
+    }
+
 }
