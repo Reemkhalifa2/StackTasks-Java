b45725051d11b7a304096da0d840b03039803d8c
ReemALadawi <aladawi12reem6@gmail.com>
2026-04-25 14:32:26 +0400

    Implement a method decimalToBinary(int decimal)

--- src/DecimalToBinaryConverter.java
+++ src/DecimalToBinaryConverter.java
@@ -2,11 +2,25 @@ import java.util.Stack;
 public class DecimalToBinaryConverter {
     public static void main(String[] args) {
         Stack<Integer> reminders = new Stack<>();
-        Double n1 = 2.2;
-        Double n2 = 1.6;
-        Double n3 = 55.6;
-        Double n4 = 44.6;
-        Double n5 = 21.5;
-        Double n6 = 6.5;
+        Integer a = 12;
+        Integer b = 45;
+        Integer c = 0;
+        Integer d = -7;
+        Integer e = 78;
+        Integer f = 5;
+        System.out.println(decimalToBinary(a));
+
+    }
+    public static String decimalToBinary(int decimal){
+        Stack<Integer> binaryStack = new Stack<>();
+        String binaryString = "";
+        while(decimal !=0){
+            binaryStack.push(decimal%2);
+            decimal = decimal/2;
+        }
+        while(!binaryStack.isEmpty()){
+           binaryString += binaryStack.pop();
+        }
+        return binaryString;
     }
 }
