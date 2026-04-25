217259d029921645576d8db4d3c79badcf07a51e
ReemALadawi <aladawi12reem6@gmail.com>
2026-04-25 12:49:54 +0400

    Create 6 different decimal numbers to convert to binary

--- src/DecimalToBinaryConverter.java
+++ src/DecimalToBinaryConverter.java
@@ -2,5 +2,11 @@ import java.util.Stack;
 public class DecimalToBinaryConverter {
     public static void main(String[] args) {
         Stack<Integer> reminders = new Stack<>();
+        Double n1 = 2.2;
+        Double n2 = 1.6;
+        Double n3 = 55.6;
+        Double n4 = 44.6;
+        Double n5 = 21.5;
+        Double n6 = 6.5;
     }
 }
