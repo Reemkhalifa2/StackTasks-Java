4d3e7b652c26542f9de46b7f4b2b1c4a100f30fa
ReemALadawi <aladawi12reem6@gmail.com>
2026-04-25 12:43:36 +0400

    Add simple comments to explain stack operations and palindrome logic

--- src/StringReversalAndPalindrome.java
+++ src/StringReversalAndPalindrome.java
@@ -1,38 +1,54 @@
 import java.util.*;
+
 public class StringReversalAndPalindrome {
+
     public static void main(String[] args) {
+
+        // Test strings
         String str1 = "Hello";
-        String str2= "Hi";
+        String str2 = "Hi";
         String str3 = "all";
         String str4 = "Mom";
         String str5 = "LOL";
 
+        // Test case 1
         System.out.println("---------- Test Case 1 ----------------");
         System.out.println("String " + str1);
         System.out.println("Reversed String " + reverseString(str1));
         System.out.println("Palindrome Status  " + isPalindrome(str1));
+
+        // Test case 2
         System.out.println("---------- Test Case 2 ----------------");
         System.out.println("String " + str4);
         System.out.println("Reversed String " + reverseString(str4));
         System.out.println("Palindrome Status  " + isPalindrome(str4));
-
-
     }
-    public static String reverseString(String str){
-        Stack<Character> reverse = new Stack<>();
+
+    // Method to reverse string using stack
+    public static String reverseString(String str) {
+
+        Stack<Character> reverse = new Stack<>(); // stack to store characters
         String result = "";
-        for(int i=0 ; i<str.length(); i++){
+
+        // push each character into stack
+        for (int i = 0; i < str.length(); i++) {
             reverse.push(str.charAt(i));
         }
-        while(!reverse.isEmpty()){
-                result += reverse.pop();
-            }
-        return result;
+
+        // pop characters to build reversed string
+        while (!reverse.isEmpty()) {
+            result += reverse.pop();
+        }
+
+        return result; // return reversed string
     }
-    public static Boolean isPalindrome(String str){
-        String reversed = reverseString(str);
+
+    // Method to check if string is palindrome
+    public static Boolean isPalindrome(String str) {
+
+        String reversed = reverseString(str); // reverse the string
+
+        // compare original with reversed (ignore case)
         return reversed.equalsIgnoreCase(str);
-
     }
-
 }
