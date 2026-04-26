15e3e65d2427ea1a361f973fd15b8f61927f48dc
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-04-26 12:31:19 +0400

    Create stack for forward histor

--- src/BrowserNavigationSystem.java
+++ src/BrowserNavigationSystem.java
@@ -1,6 +1,7 @@
 import java.util.Stack;
 public class BrowserNavigationSystem {
     static Stack<String> backHistory = new Stack<>();
+    static Stack<String> forwardHistory = new Stack<>();
     public static void main(String[] args) {
 
     }
