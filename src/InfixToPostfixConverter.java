cc9be1dedcad728fb85d0d341a981859163e421c
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-04-26 16:16:07 +0400

    evaluatePostfix(String postfix) - Evaluate the postfix expression

--- src/InfixToPostfixConverter.java
+++ src/InfixToPostfixConverter.java
@@ -46,4 +46,64 @@ public class InfixToPostfixConverter {
         }
         System.out.println(postfix);
     }
+    public static Integer evaluatePostfix(String expression) {
+
+        // Check if expression is empty
+        if(expression.isEmpty()){
+            throw new RuntimeException("Empty expression!");
+        }
+
+        // Stack to store numbers
+        Stack<Integer> operandsStack = new Stack<>();
+
+        String[] exp = expression.split(" ");
+
+        for (String x : exp) {
+
+            if (Character.isDigit(x.charAt(0))) {
+                operandsStack.push(Integer.parseInt(x));
+            }
+            else {
+                // Make sure there are at least 2 numbers in stack
+                if(operandsStack.size() < 2){
+                    throw new RuntimeException("Invalid expression!");
+                }
+
+                Integer b = operandsStack.pop();
+                Integer a = operandsStack.pop();
+
+                Integer result = 0;
+
+                // Perform operation based on operator
+                if (x.equals("+")) {
+                    result = a + b;
+                }
+                else if (x.equals("-")) {
+                    result = a - b;
+                }
+                else if (x.equals("*")) {
+                    result = a * b;
+                }
+                else if (x.equals("/")) {
+                    // Check division by zero
+                    if (b == 0) {
+                        throw new ArithmeticException("Division by zero!");
+                    }
+                    result = a / b;
+                }
+                else if (x.equals("%")) {
+                    result = a % b;
+                }
+                else {
+                    // Invalid operator
+                    throw new RuntimeException("Invalid operation!");
+                }
+
+                // Push result back to stack
+                operandsStack.push(result);
+            }
+        }
+        // Final result (last element in stack)
+        return operandsStack.pop();
+    }
 }
