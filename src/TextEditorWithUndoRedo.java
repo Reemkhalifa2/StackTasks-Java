84bda21fc78a098c5005a1b832d8089a9ee64158
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-04-26 09:22:47 +0400

    redo() - Redo the last undone action

--- src/TextEditorWithUndoRedo.java
+++ src/TextEditorWithUndoRedo.java
@@ -4,10 +4,23 @@ public class TextEditorWithUndoRedo {
     static Stack<String> redoOperations = new Stack<>();
     static StringBuilder str = new StringBuilder();
     public static void main(String[] args) {
+        System.out.println(type("Heelo"));
+        System.out.println(type("Hi"));
+        System.out.println(undo());
 
     }
     public static StringBuilder type(String text){
         str.append(text);
+        undoOperations.push(text);
         return str;
+    }
+    public static StringBuilder undo(){
+        if(!str.isEmpty()){
+            String undoStr = undoOperations.pop();
+            redoOperations.push(undoStr);
+            str =str.delete(str.length()-undoStr.length(), str.length());
+        }
+        return str;
+
     }
 }
