d356f67106c3d83bc574c99f522d9f025fc08ba6
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-04-26 12:55:31 +0400

    showCurrentPage() - Display current page

--- src/BrowserNavigationSystem.java
+++ src/BrowserNavigationSystem.java
@@ -27,4 +27,7 @@ public class BrowserNavigationSystem {
             currentPage = forwardHistory.pop();
         }
     }
+    void showCurrentPage(){
+        System.out.println(currentPage);
+    }
 }
