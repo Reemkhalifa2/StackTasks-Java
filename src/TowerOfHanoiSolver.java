2eb941747b4d57b118149924fdc59020d9f5a50b
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-04-26 10:56:57 +0400

    Create three Stacks to represent the three pegs (A, B, C)

--- src/TowerOfHanoiSolver.java
+++ src/TowerOfHanoiSolver.java
@@ -1,5 +1,8 @@
 import java.util.Stack;
 public class TowerOfHanoiSolver {
+    static Stack<Integer> A = new Stack<>();
+    static Stack<Integer> B = new Stack<>();
+    static Stack<Integer> C = new Stack<>();
     public static void main(String[] args) {
 
     }
