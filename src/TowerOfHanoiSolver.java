56cde78c68a580567054ca82c153892eeb7f3b78
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-04-26 11:11:51 +0400

    moveDisk(char fromPeg, char toPeg) - Move a disk between pegs

--- src/TowerOfHanoiSolver.java
+++ src/TowerOfHanoiSolver.java
@@ -11,7 +11,34 @@ public class TowerOfHanoiSolver {
             A.push(i);
         }
     }
+    void moveDisk(char fromPeg, char toPeg){
+        Integer a =-1;
+        if(fromPeg == 'A'){
+            if(!A.isEmpty()){
+                A.pop();
+            }
+        }
+        if(fromPeg == 'B'){
+            if(!B.isEmpty()){
+                B.pop();
+            }
+        }
+        if(fromPeg == 'C'){
+            if(!C.isEmpty()){
+                C.pop();
 
+            }
+        }
+        if(toPeg == 'A'){
+            A.push(a);
+        }
+        if(fromPeg == 'B'){
+            B.push(a);
+        }if(fromPeg == 'C'){
+            C.push(a);
+        }
+
+    }
 
 
 
