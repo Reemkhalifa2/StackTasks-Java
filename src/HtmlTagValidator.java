f8923e8ef141a2eb3c0cff8f813b0c5a5d8852cd
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-04-26 10:15:54 +0400

    Extracts tags from the HTML string

--- src/HtmlTagValidator.java
+++ src/HtmlTagValidator.java
@@ -1,9 +1,23 @@
 import java.util.Stack;
 public class HtmlTagValidator {
     public static void main(String[] args) {
+        System.out.println(validateHtmlTags());
 
     }
-    public static Boolean validateHtmlTags(String html){
+    public static String validateHtmlTags(String html){
+        Stack<String> tagsStack = new Stack<>();
+        String tag ="";
+        for(int i=0; i <html.length();i++){
 
+            if(html.charAt(i) == '<'){
+                for(int j=i; j<html.length(); j++){
+                    if(html.charAt(j)=='>'){
+                        tag  = html.substring(i+1, j);
+                    }
+                }
+
+            }
+        }
+        return tag;
     }
 }
