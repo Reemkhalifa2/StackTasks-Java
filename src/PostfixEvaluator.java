049fba78191b886cc38151de4ad40e6dd1dfc383
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-04-26 09:00:31 +0400

    Handle edge cases (empty expression, invalid operators, division by zero)

--- src/PostfixEvaluator.java
+++ src/PostfixEvaluator.java
@@ -1,43 +1,59 @@
 import java.util.Stack;
+
 public class PostfixEvaluator {
     public static void main(String[] args) {
-        String exp1 = "3 4 +";
+        String exp1 = "1 2 +";
         String exp2 = "10 2 *";
         String exp3 = "5 2 -";
-        String exp4 = "8 4 /";
+        String exp4 = "8 0 /";
         String exp5 = "7 3 %";
-        String exp6 = "2 3 4 * +";
+        String exp6 = "2 3 4 * % ";
 
-        System.out.println("Result: " + evaluatePostfix(exp1));
-        System.out.println("Result: " + evaluatePostfix(exp2));
-        System.out.println("Result: " + evaluatePostfix(exp3));
-        System.out.println("Result: " + evaluatePostfix(exp4));
-        System.out.println("Result: " + evaluatePostfix(exp5));
-        System.out.println("Result: " + evaluatePostfix(exp6));
+        System.out.println(exp1 + " = " + evaluatePostfix(exp1));
+        System.out.println(exp2 + " = " + evaluatePostfix(exp2));
+        System.out.println(exp3 + " = " + evaluatePostfix(exp3));
+        System.out.println(exp4 + " = " + evaluatePostfix(exp4));
+        System.out.println(exp5 + " = " + evaluatePostfix(exp5));
+        System.out.println(exp6 + " = " + evaluatePostfix(exp6));
+
     }
     public static Integer evaluatePostfix(String expression) {
+
+        if(expression.isEmpty()){
+            throw new RuntimeException("Empty expression!");
+        }
         Stack<Integer> operandsStack = new Stack<>();
+        String[] exp = expression.split(" ");
+        for (String x : exp) {
+            if (Character.isDigit(x.charAt(0))) {
+                operandsStack.push(Integer.parseInt(x));
+            } else {
+                if(operandsStack.isEmpty()){
 
-        for(int i = 0 ; i > expression.length();i++){
-            char ch = expression.charAt(i);
-            if (Character.isDigit(ch)) {
-                operandsStack.push(Integer.parseInt(String.valueOf(ch)));
-            }
-            else {
-                int b = operandsStack.pop();
-                int a = operandsStack.pop();
-                int result = 0;
-                if(ch =='+' ) {
+                }
+                Integer b = operandsStack.pop();
+                Integer a = operandsStack.pop();
+                Integer result = 0;
+                if (x.equals("+")) {
                     result = a + b;
-                }else if(ch == '-'){ result = a - b;}
-                else if(ch == '*'){ result = a * b;}
-                else if(ch == '/'){ result = a / b;}
-
+                } else if (x.equals("-")) {
+                    result = a - b;
+                } else if (x.equals("*")) {
+                    result = a * b;
+                } else if (x.equals("/")) {
+                    if (b == 0) {
+                        throw new ArithmeticException("Division by zero!");
+                    }
+                    result = a / b;
+                } else if (x.equals("%")) {
+                    result = a % b;
+                }else{
+                    throw new RuntimeException("Invalid operation!");
+                }
                 operandsStack.push(result);
             }
         }
-
-
         return operandsStack.pop();
+
     }
 }
