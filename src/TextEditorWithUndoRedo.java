928cf4276d50a253913ec820f8190b9c1e9db6c9
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-04-26 09:10:42 +0400

    Create two Stacks: one for undo operations and one for redo operations

--- src/TextEditorWithUndoRedo.java
+++ src/TextEditorWithUndoRedo.java
@@ -1,6 +1,7 @@
 import java.util.Stack;
 public class TextEditorWithUndoRedo {
     public static void main(String[] args) {
-
+        Stack<String> undoOperations = new Stack<>();
+        Stack<String> redoOperations = new Stack<>();
     }
 }
