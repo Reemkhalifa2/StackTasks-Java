b19d592c0c99e05df353f2425a28dedef3897dee
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-04-26 16:11:25 +0400

    infixToPostfix(String infix) - Convert infix to postfix

--- src/InfixToPostfixConverter.java
+++ src/InfixToPostfixConverter.java
@@ -3,9 +3,12 @@ import java.util.Stack;
 public class InfixToPostfixConverter {
     static Stack<Character> Postfix = new Stack<>();
     public static void main(String[] args) {
+        infixToPostfix("1+5*(4*8-5)-2");
+        System.out.println(Postfix);
+
 
     }
-    static void infixToPostfix(String infix){
+    public static void infixToPostfix(String infix){
         String postfix = "";
         for(int i=0; i<infix.length(); i++){
             char ch = infix.charAt(i);
@@ -16,23 +19,31 @@ public class InfixToPostfixConverter {
                 if(ch=='('){
                     Postfix.push(ch);
                 } else if (ch == ')') {
-                    while (Postfix.peek()!='('){
+                    while (!Postfix.isEmpty() && Postfix.peek() != '('){
                         postfix += Postfix.pop();
                     }
                     Postfix.pop();
                 } else if (ch == '^') {
                     Postfix.push(ch);
                 } else if (ch == '*' || ch =='/'|| ch =='%') {
-                    if(Postfix.peek()!='^'){
-                        Postfix.push(ch);
-                    }else {
-                        if(!Postfix.isEmpty()){
-                            postfix += Postfix.pop();
-                        }
+                    while (!Postfix.isEmpty() &&
+                            (Postfix.peek() == '*' || Postfix.peek() == '/' ||Postfix.peek() == '%' || Postfix.peek() == '^')) {
+                        postfix += Postfix.pop();
+                    }
+                    Postfix.push(ch);
+                }
+                else if (ch == '+' || ch =='-') {
+                    while (!Postfix.isEmpty() && Postfix.peek() != '('){
+                        postfix += Postfix.pop();
+                    }
+                    Postfix.push(ch);
                     }
 
-                }
             }
         }
+        while(!Postfix.isEmpty()){
+            postfix += Postfix.pop();
+        }
+        System.out.println(postfix);
     }
 }
