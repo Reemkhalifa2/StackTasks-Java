ef8246adb9b2decf9256892d62a39ceadebd448b
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-04-26 08:08:22 +0400

    Task 6: Reverse a String and Check Palindrome Using Java Stac

--- src/StringReversalAndPalindrome.java
+++ src/StringReversalAndPalindrome.java
@@ -3,5 +3,17 @@ public class StringReversalAndPalindrome {
     public static void main(String[] args) {
 
     }
+    public static String isPalindrome(String str){
+        Stack<String> reverseStack = new Stack<>();
+        for(int i=0; i<str.length(); i++){
+            char ch = str.charAt(i);
+            reverseStack.push(String.valueOf(ch));
+        }
+        String reverse ="";
+        for(String s : reverse){
+            reverse +=s;
+
+        }
+    }
 
 }
