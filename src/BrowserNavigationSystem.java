e652d283a96db13100a41931a38f170765328bab
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-04-26 13:12:42 +0400

    Implement a maximum history limit

--- src/BrowserNavigationSystem.java
+++ src/BrowserNavigationSystem.java
@@ -4,6 +4,24 @@ public class BrowserNavigationSystem {
     static Stack<String> forwardHistory = new Stack<>();
     static String  currentPage ="";
     public static void main(String[] args) {
+        visitPage("aaa");
+        showHistory();
+        showCurrentPage();
+        visitPage("bbb");
+        showHistory();
+        showCurrentPage();
+        goBack();
+        showHistory();
+        showCurrentPage();
+        goForward();
+        showHistory();
+        showCurrentPage();
+        visitPage("ccc");
+        goBack();
+        visitPage("ddd");
+        showHistory();
+        showCurrentPage();
+
 
     }
     static void visitPage(String url){
@@ -15,19 +33,31 @@ public class BrowserNavigationSystem {
             currentPage = url;
         }
     }
-    void goBack(){
+    static void goBack(){
+        if(backHistory.size() >= 10){
+            backHistory.remove(0);
+        }
         if(!backHistory.isEmpty()){
             forwardHistory.push(currentPage);
             currentPage = backHistory.pop();
+        }else{
+            System.out.println("Cannot go back");
         }
     }
-    void goForward() {
+    static void goForward() {
         if(!forwardHistory.isEmpty()){
             backHistory.push(currentPage);
             currentPage = forwardHistory.pop();
+        }else{
+            System.out.println("no forward history.");
+
         }
     }
-    void showCurrentPage(){
+    static void showCurrentPage(){
         System.out.println(currentPage);
+    }
+    static void showHistory(){
+        System.out.println("Backword History: "+ backHistory);
+        System.out.println("Forward History: "+ forwardHistory);
     }
 }
