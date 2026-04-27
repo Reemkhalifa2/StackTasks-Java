687d420cc1a9f9e8fad64d4d914ae9c4d14940fa
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-04-27 12:10:46 +0400

    Task 18: Evaluate Infix Expression Directly Using Two Stacks

--- src/InfixExpressionEvaluator.java
+++ src/InfixExpressionEvaluator.java
@@ -4,42 +4,67 @@ public class InfixExpressionEvaluator {
     static Stack<Double> operandStack = new Stack<>();
     static Stack<Character> operatorStack = new Stack<>();
     public static void main(String[] args) {
+        String exp = "3+4*2";
+        System.out.println("Expression: " + exp);
+        System.out.println("Result: " + evaluate(exp));
 
     }
-    public static void evaluate(String expression){
-        for(int i= 0 ;i<expression.length(); i++){
-            char ch = expression.charAt(i);
-            if(isOperator(ch)){
-                operatorStack.push(ch);
-            } else if (ch == '(') {
-                operatorStack.push(ch);
+    public static Double evaluate(String expression){for (int i = 0; i < expression.length(); i++) {
+        char ch = expression.charAt(i);
+        if (ch == ' ') continue;
+        if (isNumber(ch)) {
+            StringBuilder num = new StringBuilder();
+            while (i < expression.length() && isNumber(expression.charAt(i))) {
+                num.append(expression.charAt(i));
+                i++;
             }
-            else if (ch == ')') {
-
-                while (operatorStack.peek() != '(') {
-                    operandStack.push(applyOperation(operandStack.pop() ,operandStack.pop(),operatorStack.pop() ));
-                }
-
-                operatorStack.pop(); // remove '('
+            i--;
+            operandStack.push(Double.parseDouble(num.toString()));
+        }
+        else if (ch == '(') {
+            operatorStack.push(ch);
+        }
+        else if (ch == ')') {
+            while (operatorStack.peek() != '(') {
+                char op = operatorStack.pop();
+                double b = operandStack.pop();
+                double a = operandStack.pop();
+                operandStack.push(applyOperation(a, b, op));
             }
-            if(isNumber(ch)){
-                if (isNumber(ch)) {
-
-                    StringBuilder num = new StringBuilder();
-
-                    while (i < expression.length() && isNumber(expression.charAt(i))) {
-                        num.append(expression.charAt(i));
-                        i++;
-                    }
-
-                    i--;
-
-                    operandStack.push(Double.parseDouble(num.toString()));
-                }
-            }
+            operatorStack.pop();
         }
 
+        else if (isOperator(ch)) {
+
+            while (!operatorStack.isEmpty()
+                    && operatorStack.peek() != '('
+                    && getPrecedence(operatorStack.peek()) >= getPrecedence(ch)) {
+
+                char op = operatorStack.pop();
+
+                double b = operandStack.pop();
+                double a = operandStack.pop();
+
+                operandStack.push(applyOperation(a, b, op));
+            }
+
+            operatorStack.push(ch);
+        }
     }
+        while (!operatorStack.isEmpty()) {
+
+            char op = operatorStack.pop();
+
+            double b = operandStack.pop();
+            double a = operandStack.pop();
+
+            operandStack.push(applyOperation(a, b, op));
+        }
+
+        return operandStack.pop();
+
+    }
+
     static double applyOperation(double a, double b, char operator) {
         switch (operator) {
             case '+': return a + b;
