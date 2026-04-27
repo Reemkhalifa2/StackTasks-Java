9adea298445fbbd48da600db1ea234963907b9da
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-04-27 15:37:24 +0400

    Rename findCelebrity to findCelebrityBruteForce for clarity

--- src/CelebrityFinder.java
+++ src/CelebrityFinder.java
@@ -13,7 +13,7 @@ public class CelebrityFinder {
         return matrix[i][j] == 1;
     }
 
-    static int findCelebrity(int n) {
+    static int findCelebrityBruteForce(int n) {
 
         for (int i = 0; i < n; i++) {
             boolean isCelebrity = true;
