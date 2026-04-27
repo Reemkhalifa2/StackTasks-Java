e2617a0d4292d6a9170a23d1886fec4d17f7288d
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-04-27 15:48:48 +0400

    Add findCelebrity method to identify the celebrity in a group

--- src/CelebrityFinder.java
+++ src/CelebrityFinder.java
@@ -1,6 +1,7 @@
 import java.util.Stack;
 public class CelebrityFinder {
     private static int[][] matrix;
+    static Stack<Integer> people = new Stack<>();
     public static void main(String[] args) {
         matrix = new int[][]{
                 {0, 1, 0},
@@ -8,12 +9,40 @@ public class CelebrityFinder {
                 {0, 1, 0}
         };
         System.out.println(findCelebrity(matrix.length));
+        System.out.println(findCelebrityBruteForce(matrix.length));
     }
+
+    private static Integer findCelebrity(int n) {
+        for(int i=0; i< n; i++){
+            people.push(i);
+        }
+        while (people.size() > 1){
+            int a = people.pop();
+            int b = people.pop();
+            if(knows(a, b)){
+                people.push(b);
+            } else {
+                people.push(a);
+            }
+        }
+        int candidate = people.pop();
+
+        for (int i = 0; i < n; i++) {
+            if (i != candidate) {
+                if (knows(candidate, i) || !knows(i, candidate)) {
+                    return -1;
+                }
+            }
+        }
+
+        return candidate;
+    }
+
     static boolean knows(int i, int j) {
         return matrix[i][j] == 1;
     }
 
-    static int findCelebrityBruteForce(int n) {
+    static Integer findCelebrityBruteForce(int n) {
 
         for (int i = 0; i < n; i++) {
             boolean isCelebrity = true;
