7c5fa339c51f3d6df2d8241662f52fc2f2c65450
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-04-27 08:53:59 +0400

    findNextSmallerElement(int[] arr) - Find next smaller element

--- src/NextGreaterElementFinder.java
+++ src/NextGreaterElementFinder.java
@@ -21,5 +21,31 @@ public class NextGreaterElementFinder {
         }
         return result;
     }
+    public static int[] findNextGreaterElementCircular(int[] arr) {
+        int[] result = new int[arr.length];
+        Stack<Integer> st = new Stack<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            result[i] = -1;
+        }
+        for (int i = 2 * arr.length - 1; i >= 0; i--) {
+
+            int current = arr[i % arr.length];
+
+            while (!st.isEmpty() && current >= arr[st.peek()]) {
+                st.pop();
+            }
+            if (i < arr.length) {
+                if (!st.isEmpty()) {
+                    result[i] = arr[st.peek()];
+                }
+            }
+
+            st.push(i % arr.length);
+        }
+
+        return result;
+    }
+    displayResults(int[] arr, int[] result
 
 }
