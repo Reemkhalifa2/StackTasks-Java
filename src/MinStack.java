8ce292df0a6c3c1f3f56a2ec9ad43ce1e3388297
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-04-27 10:09:02 +0400

    Task 16: Implement a Stack that Supports Get Minimum in O(1) Time

--- src/MinStack.java
+++ src/MinStack.java
@@ -6,6 +6,13 @@ public class MinStack {
     static Stack<Integer> minStack = new Stack<>();
 
     public static void main(String[] args) {
+        push(5);
+        push(2);
+        push(8);
+        pop();
+        push(1);
+
+        display();
 
 
     }
@@ -28,10 +35,36 @@ public class MinStack {
         }
 
         minStack.pop();
-
-
         return mainStack.pop();
     }
+
+    static Integer peek() {
+
+        if (mainStack.isEmpty()) {
+            System.out.println("Stack is empty");
+            return -1;
+        }
+
+        return mainStack.peek();
+    }
+    static int getMin() {
+
+        if (minStack.isEmpty()) {
+            System.out.println("stack is empty");
+            return -1;
+        }
+
+        return minStack.peek();
+    }
+
+    static void display() {
+
+        System.out.println("Main Stack: " + mainStack);
+        System.out.println("Min Stack : " + minStack);
+        System.out.println("Current Min: " + getMin());
+        System.out.println("----------------------");
+    }
+
 
 
 }
