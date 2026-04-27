d0e787baf8ba3e189589d634432ac75b869f1285
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-04-27 09:13:04 +0400

    create outputStack

--- src/QueueUsingTwoStacks.java
+++ src/QueueUsingTwoStacks.java
@@ -1,6 +1,7 @@
 import java.util.Stack;
 public class QueueUsingTwoStacks {
     static Stack<Integer> inputStack = new Stack<>();
+    static Stack<Integer> outputStack = new Stack<>();
     public static void main(String[] args) {
 
     }
