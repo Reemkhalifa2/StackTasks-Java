0b795c167e864360ed321963f045490cfa0e1bb1
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-04-27 15:24:03 +0400

    Implement celebrity finding algorithm in CelebrityFinder

--- src/CelebrityFinder.java
+++ src/CelebrityFinder.java
@@ -1,6 +1,36 @@
 import java.util.Stack;
 public class CelebrityFinder {
+
+    private static double[][] matrix;
+
     public static void main(String[] args) {
-        
+        Integer[][] matrix = {
+                {0, 1, 0},
+                {0, 0, 0},
+                {0, 1, 0}
+        };
+        System.out.println(findCelebrity(matrix));
+
     }
+    static boolean knows(int i, int j) {
+        return matrix[i][j] == 1;
+    }
+    static Integer findCelebrity(int n) {
+        for (int i = 0; i < n; i++) {
+            boolean isCelebrity = true;
+            for (int j = 0; j < n; j++) {
+                if (i != j && (knows(i, j) || !knows(j, i))) {
+                    isCelebrity = false;
+                    break;
+                }
+            }
+            if (isCelebrity) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    
+
 }
