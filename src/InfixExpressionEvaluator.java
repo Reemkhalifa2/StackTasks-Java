9edc614d06bccb9d581bf9bd2b3e1028226a02e8
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-04-27 11:32:11 +0400

    create operatorStack

--- src/InfixExpressionEvaluator.java
+++ src/InfixExpressionEvaluator.java
@@ -2,6 +2,7 @@
 import java.util.Stack;
 public class InfixExpressionEvaluator {
     static Stack<Integer> operandStack = new Stack<>();
+    static Stack<Integer> operatorStack = new Stack<>();
     public static void main(String[] args) {
 
     }
