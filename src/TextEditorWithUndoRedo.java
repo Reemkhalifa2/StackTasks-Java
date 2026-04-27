e4693557a6135a0ba893f0fb56144ec7d034cd7c
ReemALadawi <aladawi12reem6@gmail.com>
2026-04-27 19:27:56 +0400

    type(String text) - Add new text

--- src/TextEditorWithUndoRedo.java
+++ src/TextEditorWithUndoRedo.java
@@ -1,12 +1,3 @@
 import java.util.Stack;
 public class TextEditorWithUndoRedo {
-    public static void main(String[] args) {
-           }
-    public static StringBuilder type(String text){
-        StringBuilder sb = new StringBuilder();
-        sb.append(text);
-        return sb;
-    }
-    
-
 }
