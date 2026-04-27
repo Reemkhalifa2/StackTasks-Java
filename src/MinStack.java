96c345275396fc95a23cbf7cf519917720d7db73
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-04-27 09:41:30 +0400

    create minStack

--- src/MinStack.java
+++ src/MinStack.java
@@ -1,6 +1,7 @@
 import java.util.Stack;
 public class MinStack {
     static Stack<Integer> mainStack = new Stack<>();
+    static Stack<Integer> minStack = new Stack<>();
     public static void main(String[] args) {
 
     }
