be10306484e00cd601b3ddcad48a43d7bea9fac3
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-04-27 14:26:09 +0400

    Refactor StockSpanCalculator to implement span calculation logic

--- src/StockSpanCalculator.java
+++ src/StockSpanCalculator.java
@@ -1,11 +1,23 @@
 import java.util.Stack;
 public class StockSpanCalculator {
-    Stack<Integer> indices = new Stack<>();
+    static Stack<Integer> indices = new Stack<>();
     public static void main(String[] args) {
 
     }
     static void calculateSpan(int[] prices){
-        
+        for(int i= 0; i<prices.length; i++){
+            while(!indices.isEmpty() && prices[indices.peek()] <= prices[i]){
+                    indices.pop();
+                }
+                int span =-1;
+            if(indices.isEmpty() ){
+                i+=1;
+            } else{
+                i-= indices.peek();
+            }
+                indices.push(i);
+
+                    }
     }
 
 }
