cc2dce603c65606e3f4fc28f041aa69bc6d0f6d8
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-04-27 10:11:23 +0400

    Create a temporary Stack for sorting

--- src/StackSorter.java
+++ src/StackSorter.java
@@ -1,6 +1,7 @@
 import java.util.Stack;
 public class StackSorter {
     static Stack<Integer> unsortedStack = new Stack<>();
+    static Stack<Integer>  temporaryStack = new Stack<>();
     public static void main(String[] args) {
 
     }
