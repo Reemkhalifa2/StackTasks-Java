f78a452aa84c10744e3adaf1801e98de0c56a63a
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-04-27 10:04:49 +0400

    Implement push method for main stack

--- src/MinStack.java
+++ src/MinStack.java
@@ -5,5 +5,21 @@ public class MinStack {
     static Stack<Integer> mainStack = new Stack<>();
     static Stack<Integer> minStack = new Stack<>();
 
+    public static void main(String[] args) {
+
+
+    }
+
+    static void push(int value) {
+        mainStack.push(value);
+
+        if (minStack.isEmpty() || value <= minStack.peek()) {
+            minStack.push(value);
+        } else {
+            minStack.push(minStack.peek());
+        }
+
+    }
+
 
 }
