91ae7ed3bacf47956af68a84f007deb0bb482508
Reem Khalifa Aladawi <aladawi12reem6@gmail.com>
2026-04-28 08:21:15 +0400

    Task 9: Implement Undo/Redo Functionality Using Two Stack

--- src/TextEditorWithUndoRedo.java
+++ src/TextEditorWithUndoRedo.java
@@ -1,4 +1,5 @@
 import java.util.Stack;
+//Task 9: Implement Undo/Redo Functionality Using Two Stack
 public class TextEditorWithUndoRedo {
     static Stack<String> undoOperations = new Stack<>();
     static Stack<String> redoOperations = new Stack<>();
